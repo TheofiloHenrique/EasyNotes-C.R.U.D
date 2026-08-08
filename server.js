02a8bce5a9a867090eb12f900693276d5ddc4cf0
Theofilo Henrique <theohenriquee01@gmail.com>
2026-08-08 19:50:41 -0300

    fix: Alterando rotas e deixando de rastrear o arquivo db

--- server.js
+++ server.js
@@ -2,61 +2,75 @@ import express, { json } from 'express'
 import db from './database.js'
 
 const app = express()
-
 app.use(express.json())
-
-// const notes = [{
-//     id:1,
-//     title:"First note",
-//     content:"Agora sim estamos vendo algo que digitei aqui que ninguem liga pqp",
-// }]
 const port = 3000;
 
-//GET
+//ROTA GET
 app.get('/notes', (req,res) =>{
     const notes = db.prepare('SELECT * FROM notes').all()
     res.json(notes)
 })
 
-//GET PARAMS
+//ROTA GET COM PARAMS
 app.get('/notes/:id', (req,res) =>{
     const id = Number(req.params.id)
-    const noteId = notes.find(note => note.id === id)
 
-    res.json(noteId)
+    const note = db.prepare(`
+        SELECT * FROM notes WHERE id = ?
+    `).get(id);
+
+    if (!note) return res.status(404).json({ error: 'Note Not Found!'});
+    
+    res.json(note);
 })
 
-//POST
+//ROTA POST
 app.post('/notes',(req,res)=>{
-    const note = 
-    {
-        id: notes.length + 1,
-        title: req.body.title,
-        content: req.body.content,
-    }
+    const { title, content } = req.body;
 
-    notes.push(note)
-    res.json(note)
+    const result = db.prepare(`
+        INSERT INTO notes (title, content)
+        VALUES (?, ?)
+    `).run(title, content)
+
+    const note = db.prepare(`
+        SELECT * FROM notes WHERE id = ?
+    `).get(result.lastInsertRowid)
+
+    res.status(201).json(note)
 })
 
-//PUT
+//ROTA PUT
 app.put('/notes/:id', (req, res) =>{
-    const id = Number(req.params.id)
-    const noteId = notes.find(note => note.id === id)
+    const id = Number(req.params.id);
+    const { title, content } = req.body;
 
-    noteId.title = req.body.title
-    noteId.content = req.body.content
+    const result = db.prepare(`
+        UPDATE notes
+        SET title = ?, content = ?
+        WHERE id = ?
+    `).run(title, content, id)
 
-    res.json(noteId)
+    if (result.changes === 0) return res.status(404).json({error: 'Note Not Found'});
+    
+    const note = db.prepare(`
+        SELECT * FROM notes WHERE id = ?
+    `).get(id);
+
+    res.json(note);
 })
 
-//DELETE
+//ROTA DELETE
 app.delete('/notes/:id', (req, res) => {
-    const id = req.params.id
-    const noteIndex = notes.findIndex(note => note.id === id)
+    const id = Number(req.params.id)
 
-    notes.splice(noteIndex, 1)
-    res.send("Nota excluida")
+    const result = db.prepare(`
+        DELETE FROM notes WHERE id = ?
+    `).run(id)
+
+    if (result.changes === 0) return res.status(404).json({error: 'Nota não encontrada'})
+    
+    res.status(204).send()
 })
 
 app.listen(port, () => console.log(`Motores ligados na porta ${port}! xD`))
